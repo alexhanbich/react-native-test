aa99492a7330040d6eeedd956a2b60cf2f28cfa5
Alex Kang <alexhanabich@gmail.com>
2022-08-04 15:44:51 +0900

    test react-native

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [outputList, setOutput] = useState([]);
+
+  function inputHandler(input) {
+    setInput(input)
+  }
+
+  function outputHandler() {
+    setOutput(currentList => [...currentList, input]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput 
+        style={styles.inputText} 
+        placeholder='Type something here...'
+        onChangeText={inputHandler}
+        />
+        <Button title='CLICK ME!!' onPress={outputHandler} />
+      </View>
+      <View style={styles.outputContainer}>
+        {outputList.map((text) => <Text key={text}>{text}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    flex: 1
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: '#000000',
+    width: '80%',
+    marginRight: 10,
+    padding: 10
+  },
+  outputContainer: {
+    flex: 6
+  }
 });
